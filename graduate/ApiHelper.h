2efdf635e803fbcf341cd4fb06c54f6f0a85fe39
wk121250151 <kswangk@163.com>
2015-03-22 23:10:21 +0800

    Fix Bug

--- graduate/ApiHelper.h
+++ graduate/ApiHelper.h
@@ -9,8 +9,8 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import "ToolUtils.h"
-//#define BASEURL @"http://114.215.196.179:8080/gs/mobile"
-#define BASEURL @"http://192.168.1.130:8080/gs/mobile"
+#define BASEURL @"http://114.215.196.179:8080/gs/mobile"
+//#define BASEURL @"http://192.168.1.130:8080/gs/mobile"
 @protocol ApiDelegate<NSObject>
 @required
 - (void)dispos:(NSDictionary*) data functionName:(NSString*)names;
